125beac861b05ef54dbd35677e4b4f169478ea08
DevGS23 <167510531+DevGS23@users.noreply.github.com>
2025-02-16 23:53:47 +0000

    Add GraphQL schema and resolvers for User authentication

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,10 +1,9 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { User, Meetup, Match } = require('../models');
+const { User } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
-    // Existing queries
     me: async (parent, args, context) => {
       if (context.user) {
         return User.findOne({ _id: context.user._id });
@@ -14,61 +13,11 @@ const resolvers = {
     users: async () => {
       return User.find();
     },
-    user: async (parent, { username }) => {
-      return User.findOne({ username });
-    },
-    meetups: async () => {
-      return Meetup.find().populate('creator').populate('currentPlayers');
-    },
-    meetup: async (parent, { _id }) => {
-      return Meetup.findOne({ _id }).populate('creator').populate('currentPlayers');
-    },
-    myMeetups: async (parent, args, context) => {
-      if (context.user) {
-        return Meetup.find({
-          $or: [
-            { creator: context.user._id },
-            { currentPlayers: context.user._id }
-          ]
-        }).populate('creator').populate('currentPlayers');
-      }
-      throw new AuthenticationError('You need to be logged in!');
-    },
-    // New match queries
-    matches: async () => {
-      return Match.find()
-        .populate('players')
-        .populate('teams.team1')
-        .populate('teams.team2')
-        .populate('meetup');
-    },
-    match: async (parent, { _id }) => {
-      return Match.findOne({ _id })
-        .populate('players')
-        .populate('teams.team1')
-        .populate('teams.team2')
-        .populate('meetup');
-    },
-    userMatches: async (parent, { userId }) => {
-      return Match.find({ players: userId })
-        .populate('players')
-        .populate('teams.team1')
-        .populate('teams.team2')
-        .populate('meetup');
-    },
-    meetupMatches: async (parent, { meetupId }) => {
-      return Match.find({ meetup: meetupId })
-        .populate('players')
-        .populate('teams.team1')
-        .populate('teams.team2')
-        .populate('meetup');
-    }
   },
-
+  
   Mutation: {
-    // Existing mutations
-    addUser: async (parent, args) => {
-      const user = await User.create(args);
+    addUser: async (parent, { username, email, password, skillLevel }) => {
+      const user = await User.create({ username, email, password, skillLevel });
       const token = signToken(user);
       return { token, user };
     },
@@ -76,7 +25,7 @@ const resolvers = {
       const user = await User.findOne({ email });
 
       if (!user) {
-        throw new AuthenticationError('Incorrect credentials');
+        throw new AuthenticationError('No user found with this email address');
       }
 
       const correctPw = await user.isCorrectPassword(password);
@@ -88,71 +37,7 @@ const resolvers = {
       const token = signToken(user);
       return { token, user };
     },
-    createMeetup: async (parent, { input }, context) => {
-      if (context.user) {
-        const meetup = await Meetup.create({
-          ...input,
-          creator: context.user._id,
-          currentPlayers: [context.user._id]
-        });
-        return meetup.populate('creator').populate('currentPlayers');
-      }
-      throw new AuthenticationError('You need to be logged in!');
-    },
-    joinMeetup: async (parent, { meetupId }, context) => {
-      if (context.user) {
-        const meetup = await Meetup.findOneAndUpdate(
-          { _id: meetupId },
-          { $addToSet: { currentPlayers: context.user._id } },
-          { new: true }
-        ).populate('creator').populate('currentPlayers');
-        return meetup;
-      }
-      throw new AuthenticationError('You need to be logged in!');
-    },
-    // New match mutations
-    createMatch: async (parent, { players, team1, team2, scores, winner, meetupId, location }, context) => {
-      if (context.user) {
-        const match = await Match.create({
-          players,
-          teams: {
-            team1,
-            team2
-          },
-          scores,
-          winner,
-          meetup: meetupId,
-          location
-        });
-        return match
-          .populate('players')
-          .populate('teams.team1')
-          .populate('teams.team2')
-          .populate('meetup');
-      }
-      throw new AuthenticationError('You need to be logged in!');
-    },
-    updateMatchScore: async (parent, { matchId, scores, winner }, context) => {
-      if (context.user) {
-        const match = await Match.findOneAndUpdate(
-          { _id: matchId },
-          {
-            $set: {
-              scores,
-              winner
-            }
-          },
-          { new: true }
-        )
-          .populate('players')
-          .populate('teams.team1')
-          .populate('teams.team2')
-          .populate('meetup');
-        return match;
-      }
-      throw new AuthenticationError('You need to be logged in!');
-    }
-  }
+  },
 };
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -6,47 +6,6 @@ const typeDefs = gql`
     username: String
     email: String
     skillLevel: String
-    meetups: [Meetup]
-  }
-
-  type Meetup {
-    _id: ID
-    title: String
-    creator: User
-    location: String
-    date: String
-    time: String
-    skillLevel: String
-    maxPlayers: Int
-    currentPlayers: [User]
-    status: String
-    createdAt: String
-  }
-
-  type Team {
-    players: [User]
-    scores: [Int]
-  }
-
-  type Match {
-    _id: ID
-    date: String
-    players: [User]
-    teams: TeamData
-    scores: ScoreData
-    winner: String
-    meetup: Meetup
-    location: String
-  }
-
-  type TeamData {
-    team1: [User]
-    team2: [User]
-  }
-
-  type ScoreData {
-    team1: [Int]
-    team2: [Int]
   }
 
   type Auth {
@@ -54,54 +13,14 @@ const typeDefs = gql`
     user: User
   }
 
-  input MeetupInput {
-    title: String!
-    location: String!
-    date: String!
-    time: String!
-    skillLevel: String
-    maxPlayers: Int!
-  }
-
-  input ScoreInput {
-    team1: [Int]!
-    team2: [Int]!
-  }
-
   type Query {
     me: User
     users: [User]
-    user(username: String!): User
-    meetups: [Meetup]
-    meetup(_id: ID!): Meetup
-    myMeetups: [Meetup]
-    matches: [Match]
-    match(_id: ID!): Match
-    userMatches(userId: ID!): [Match]
-    meetupMatches(meetupId: ID!): [Match]
   }
 
   type Mutation {
+    addUser(username: String!, email: String!, password: String!, skillLevel: String): Auth
     login(email: String!, password: String!): Auth
-    addUser(username: String!, email: String!, skillLevel: String): Auth
-    createMeetup(input: MeetupInput!): Meetup
-    joinMeetup(meetupId: ID!): Meetup
-    cancelMeetup(meetupId: ID!): Meetup
-    updateMeetupStatus(meetupId: ID!, status: String!): Meetup
-    createMatch(
-      players: [ID]!
-      team1: [ID]!
-      team2: [ID]!
-      scores: ScoreInput!
-      winner: String!
-      meetupId: ID
-      location: String!
-    ): Match
-    updateMatchScore(
-      matchId: ID!
-      scores: ScoreInput!
-      winner: String!
-    ): Match
   }
 `;
 
